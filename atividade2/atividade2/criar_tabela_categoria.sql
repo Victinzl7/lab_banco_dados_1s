ebf324c6a767c538a143d13b298d0bd338cbbc1a
Victinzl7 <victor.dc.silva15@gmail.com>
2026-03-14 14:02:07 -0300

    Delete atividade2/atividade2 directory

--- atividade2/atividade2/criar_tabela_categoria.sql
+++
@@ -1,9 +0,0 @@
-USE atividade2;
-CREATE TABLE Categoria (
-    id_categoria INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(128) NOT NULL UNIQUE,
-    descricao TEXT,
-    data_cadastro TIMESTAMP,
-    data_atualizacao TIMESTAMP
-);
-show tables;
